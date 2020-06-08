f0962dd688799eb6369fdeea726b217b3d811b6f
maxaakre <maxaakre@gmail.com>
2020-06-08 13:34:38 +0200

    Maked views and started at staff

---
+++ src/views/Staff.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <img src="../assets/logo.png" alt="logo" />
+      <h2>Where it´s @</h2>
+      <h3>STAFF</h3>
+      <div class="input">
+        <input type="text" class="ticketnumber" value="<Biljett nummer" />
+        <button>Verify ticket</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
