a36b8a09ee2c4e187af5be18e42f0315c61ba0ef
Patrick Carragher <carragher@live.se>
2020-06-09 19:07:07 +0200

    Fixed Bad css and mad staff scoped

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,123 +1,120 @@
 <template>
-  <div>
+  <div class="wrapper">
+    <div class="list"></div>
+    <div class="form">
+      <h2>Add Event</h2>
 
-    <div class="wrapper">
-
-      <div class="list"></div>
-
-      <div class="form">
-        <h3>Add event</h3>
-
-        <div class="input">
-
-          
-
-          <input type="text" class="wideForm" placeholder="Name"/>
-          <input type="text" class="wideForm" placeholder="Where"/>
-          <input type="text" class="wideForm" placeholder="Date"/>
-
-          
-          
-          <div class="textHalfs">
-
-            <input type="text" placeholder="From time" class="inputHalfTime"/>
-            <input type="text" placeholder="To time" class="inputHalfTime"/>
-            <input type="text" class="ticketCash" placeholder="Tickets"/>
-            <input type="text" class="ticketCash" placeholder="Price"/>
-
-          </div>
-
-          
-
-            
-
-          
-
+        <div class="inputWide">
+          <input type="text" class="wide" placeholder="Name">
+          <input type="text" class="wide" placeholder="Where">
+          <input type="text" class="wide" placeholder="Date">
         </div>
 
-      </div>
+        <div class="inputQuad">
+          <input type="text" class="half" placeholder="From Time">
+          <input type="text" class="half" placeholder="To Time">
+          <input type="text" class="half">
+          <input type="text" class="half">
+        </div>
+
+        <button>Add the Event</button>
+
 
     </div>
-
   </div>
 </template>
 
 <script>
-export default {
-  name: "Admin",
-  components: {}
-};
+export default {};
 </script>
 
-<style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100&display=swap');
-// All styling here will overwrite App.vue!
-body {
-  margin: 0;
-  padding: 0;
-}
-
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;500&display=swap');
 .wrapper {
-  background: #231f42;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background: #231F42;
   height: 100vh;
-  margin: auto;
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
+  
 
   .list {
-    position: absolute;
     width: 732px;
     height: 313px;
-    left: 130px;
-    top: 202px;
-
-    background: rgba(0, 0, 0, 0.3);
     border: 1px solid #000000;
-    box-sizing: border-box;
+    background: rgba(0, 0, 0, 0.3);
   }
   .form {
-    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 20px 50px;
+    
     width: 432px;
-    height: 378px;
-    left: 878px;
-    top: 202px;
-
     background: rgba(0, 0, 0, 0.3);
-    .input{
+    h2{
+      color: #F56B9A;
+      font-family: Fira Sans;
+      font-style: normal;
+      font-weight: bold;
+      
+    }
+
+    .inputWide{
       display: flex;
       flex-direction: column;
-      color: rgba(255, 255, 255, 0.6);
-        font-family: Fira Sans;
-
-      .wideForm{
-        width: 90%;
-        margin-bottom:13px;
-        margin-left: 18px;
+      width: 100%;
+      
+      
+      .wide{
         height: 40px;
-        background-color: transparent;
-        border-radius: 3px;
+        background: transparent;
+        margin-bottom: 12px;
         border: 1px solid rgba(255, 255, 255, 0.7);
-        box-sizing: border-box;
+        border-radius: 3px;
+      }
+      .wide::placeholder {
+        font-family: Fira Sans;
+        font-style: normal;
+        font-weight: 200;
         
       }
-
-
-      .textHalfs{
+    }
+    .inputQuad{
+     
+      margin: 0 auto -13px;
       
-
-        .inputHalfTime,.ticketCash{
-          height: 40px;
-        background-color: transparent;
-        border-radius: 3px;
+      .half{
+        height: 40px;
+        width: calc(100% / 2 - 16px);
+        background: transparent;
+        display: inline-block;
         border: 1px solid rgba(255, 255, 255, 0.7);
-        
+        border-radius: 3px;
+        margin-right: 20px;
+        margin-bottom: 13px;
+
+        &:nth-child(2n){
+          margin-right: 0;
         }
 
-        
-
+        .half::placeholder {
+        font-family: Fira Sans;
+        font-style: normal;
+        font-weight: 200;
         
       }
+      }
+    }
+
+    button{
+        height: 40px;
+        width: calc(100% / 2 - 16px);
+        background: #F56B9A;
+        color: white;
+        border: 1px solid rgba(255, 255, 255, 0.7);
+        border-radius: 3px;
+
     }
   }
 }
